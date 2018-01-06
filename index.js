4379bbdc5d1b93191cb4c9973afc9686e7ed8085
Ali Karchoud <karchoud.ali@gmail.com>
2018-01-06 07:04:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -121,6 +121,12 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+   if(e.which==RIGHT_ARROW){
+     moveDodgerRight();
+   }
+   else if (e.which==LEFT_ARROW) {
+     moveDodgerLeft();
+   }
 }
 
 function moveDodgerLeft() {
