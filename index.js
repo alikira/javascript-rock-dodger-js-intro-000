6755d20342b6ad2506283a8b5d53dd2c94d3483f
Ali Karchoud <karchoud.ali@gmail.com>
2018-01-06 07:16:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -148,6 +148,11 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   window.requestAnimationFrame(function(){
+     if(dodgerRightEdge>3){
+       DODGER.style.left=`${left + 4}px`;
+     }
+   })
 }
 
 /**
